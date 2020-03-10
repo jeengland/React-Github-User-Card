651fa426c8e9016965d33192e13824d0b6af12e8
unknown <jacob.e.england@gmail.com>
2020-03-10 10:40:17 -0500

    Initialized top level app component with API call

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
-const App = () => {
-    return (
-        <h1>Hello World</h1>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            data: []
+        }
+    }
+    componentDidMount() {
+        axios
+            .get('https://api.github.com/users/jeengland')
+            .then((response) => this.setState({ data: response.data }))
+            .catch((error) => console.error(error))
+    }
+    render() {
+       return (
+          <h1>Hello World</h1>
+       )
+    }
 }
 
 export default App;
